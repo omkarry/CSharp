467534a496b6e9d8b439d219dbf2f234b0f102b1
omkarry <yadavomkar505.com>
2023-02-28 19:11:31 +0530

    Initial Commit

---
+++ FileOperation.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace FileHandling
+{
+	class FileOperation
+	{
+        public static void CopyFile()
+        {
+            string? name, fileLocation, filePath, overwriteFile;
+
+            try
+            {
+                Console.WriteLine("\nEnter the location :");
+                fileLocation = Console.ReadLine();
+
+                Console.WriteLine("\nEnter file name: (Text file only)");
+                name = Console.ReadLine();
+                if (name is null or "" || fileLocation is null or "")
+                    Console.WriteLine("\nName or Location is empty");
+                else if (Path.GetExtension(name) is not ".txt")
+                    Console.WriteLine("\nInvalid file type. Only text (.txt) file is allowed");
+                else
+                {
+                    if (fileLocation.EndsWith("\\"))
+                        filePath = fileLocation + name;
+                    else
+                        filePath = fileLocation + "\\" + name;
+                    //If file exist then it will be overwrite
+                    if (File.Exists(filePath))
+                    {
+                        Console.WriteLine("File is already exist at this location.");
+                        Console.WriteLine("Do you want to overwrite this file?");
+                        overwriteFile = Console.ReadLine() ?? throw new ArgumentException();
+                        if (overwriteFile.ToLower() is "yes" or "y")
+                        {
+                            CreateFile(filePath);
+                        }
+                    }
+                    else
+                        CreateFile(filePath);
+
+                    Console.WriteLine("\nEnter the file with location which you want to copy:");
+                    string? copyPath = Console.ReadLine();
+                    if (File.Exists(copyPath))
+                        File.Copy(copyPath, filePath, true);
+                    else
+                        throw new FileNotFoundException("\nFile not found to copy");
+                }
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Directory not Found");
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                Console.WriteLine(uae.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void ReadData()
+        {
+            try
+            {
+                string? path;
+                Console.WriteLine("\nEnter the file name with path:");
+                path = Console.ReadLine();
+                if (path is null or "")
+                    Console.WriteLine("\nPath is empty");
+                else if (Path.GetExtension(path) is not ".txt")
+                    Console.WriteLine("\nInvalid file type. Only text (.txt) file is allowed");
+                else
+                {
+                    string text = File.ReadAllText(path);
+                    Console.WriteLine("\nText in the file {0}: \n{1}", Path.GetFileName(path), text);
+
+                    Console.WriteLine("\nWhich word you want to replace?");
+                    string? wordToReplace = Console.ReadLine() ?? throw new ArgumentNullException();
+
+                    Console.WriteLine("{0} replace this word with: ", wordToReplace);
+                    string? replaceWordWith = Console.ReadLine();
+                    text = text.Replace(wordToReplace, replaceWordWith);
+                    File.WriteAllText(path, text);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Directory not found");
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                Console.WriteLine(uae.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void ReadLastLine()
+		{
+            try
+            {
+                string? path;
+                Console.WriteLine("\nEnter the file name with:");
+                path = Console.ReadLine();
+                //FileNotFoundException can be handled with
+                //if(!File.Exists(path)
+                //    Console.WriteLine("File not found");
+                if (path is null or "") 
+                    Console.WriteLine("\nPath is empty");
+                else if (Path.GetExtension(path) is not ".txt")
+                    Console.WriteLine("\nInvalid file type. Only text (.txt) file is allowed");
+                else
+                {
+                    string lastLine = File.ReadLines(path).Last();
+                    Console.WriteLine("\nLast line of the file {0}: \n{1}", Path.GetFileName(path), lastLine);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Directory not found");
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                Console.WriteLine(uae.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+		}
+
+        public static void CreateFile(string path)
+        {
+            var file = File.CreateText(path);
+            Console.WriteLine("\nFile is created at {0}", path);
+            file.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace FileHandling
+{
+    class Program
+    {
+        private string? choice;
+        public static void Main(string[] args)
+        {
+            Program program = new();
+            FileOperation fileOperation = new FileOperation();
+
+            do
+            {
+                Console.WriteLine("\n-------------------------------");
+                Console.WriteLine("\t\tFile Operations");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("1. Copy file");
+                Console.WriteLine("2. Read data from a file and replace a word");
+                Console.WriteLine("3. Read last line from a file");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("or enter exit to stop\n");
+
+                program.choice = Console.ReadLine();
+
+                switch (program.choice)
+                {
+                    case "1":
+                        FileOperation.CopyFile();
+                        break;
+
+
+                    case "2":
+                        FileOperation.ReadData();
+                        break;
+
+                    case "3":
+                        FileOperation.ReadLastLine();
+                        break;
+
+                }
+            } while (program.choice.ToLower() != "exit");
+        }
+    }
+}
