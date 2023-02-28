4bacaf69ae993a22cc591ee04287ddda6814ef4a
omkarry <yadavomkar505.com>
2023-02-28 19:16:03 +0530

    Changes after review

--- FileOperation.cs
+++ FileOperation.cs
@@ -1,7 +1,4 @@
 ﻿using FileHandling.Exceptions;
-using System;
-using System.IO;
-using System.Runtime.InteropServices;
 
 namespace FileOperations
 {
@@ -21,12 +18,16 @@ namespace FileOperations
                     CheckFilePath(path, fileExtension);
                 }
                 else
-                    throw new InvalidFileTypeException("Invalid File Type");
+                    throw new InvalidFileTypeException("\nInvalid File Type");
             }
             catch (InvalidFileTypeException e)
             {
                 Console.WriteLine(e.Message);
-                Console.WriteLine("File type should be png/txt/xls/jpg");
+                Console.WriteLine("\nFile type should be png/txt/xls/jpg");
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                Console.WriteLine(uae.Message);
             }
             catch (Exception e)
             {
@@ -44,18 +45,25 @@ namespace FileOperations
 
             if (File.Exists(path))
             {
-                Console.WriteLine("File added successfully");
-                files.Add(path);
+                if (files.Contains(path))
+                {
+                    Console.WriteLine("\nFile is already added");
+                }
+                else
+                {
+                    files.Add(Path.GetFullPath(path));
+                    Console.WriteLine("\nFile added successfully");
+                }
             }
             else
             {
-                Console.WriteLine("File not found..");
-                Console.WriteLine("Do you want to create new file?(yes/no)");
+                Console.WriteLine("\nFile not found..");
+                Console.WriteLine("\nDo you want to create new file?(yes/no)");
                 string? newFile = Console.ReadLine();
                 if (newFile.ToLower() is "yes" or "y")
                 {
                     if (fileExtention.ToLower() is ".jpg" or ".png")
-                        Console.WriteLine("{0} type file cannot be created", fileExtention);
+                        Console.WriteLine("\n{0} type file cannot be created", fileExtention);
                     else
                         CreateFile(path);
                 }
@@ -66,7 +74,8 @@ namespace FileOperations
         {
             using (FileStream createFile = new FileStream(path, FileMode.CreateNew))
             {
-                Console.WriteLine("New file is created at {0}", path);
+                path = Path.GetFullPath(path);
+                Console.WriteLine("\nNew file is created at {0}", path);
                 files.Add(path);
             }
         }
@@ -77,7 +86,9 @@ namespace FileOperations
             if (files.Contains(path))
             {
                 if (fileExtension.ToLower() is ".jpg" or ".png" or ".xls")
-                    Console.WriteLine("Unable to write {0} file", fileExtension);
+                    Console.WriteLine("\nCannot write to {0} file, please select text(.txt) file", fileExtension);
+                //else if (fileExtension.ToLower() is ".xls")
+                //    writeExcel(path);
                 else
                 {
                     FileStream fileStream = new FileStream(path, FileMode.Append, FileAccess.Write);
@@ -85,14 +96,14 @@ namespace FileOperations
                     {
                         do
                         {
-                            Console.WriteLine("Enter data you want to add in the file {0}", Path.GetFileName(path));
+                            Console.WriteLine("\nEnter data you want to add in the file {0}", Path.GetFileName(path));
                             string? writeToFile = Console.ReadLine();
                             streamWriter.WriteLine(writeToFile);
-                            Console.WriteLine("Do you want to write more?(yes/no)");
+                            Console.WriteLine("\nDo you want to write more?(yes/no)");
                             writeMore = Console.ReadLine();
                             if (!(writeMore.ToLower() is "yes" or "no"))
                             {
-                                Console.WriteLine("Closing file...");
+                                Console.WriteLine("\nClosing file...");
                             }
                         } while (writeMore == "yes");
                     }
@@ -100,8 +111,64 @@ namespace FileOperations
             }
             else
             {
-                Console.WriteLine("File is not added. Please select files from uploaded or add new file.");
+                Console.WriteLine("\nFile is not added. Please select files from uploaded or add new file.");
             }
         }
+        //Public void writeExcel(string path)
+        //{
+        //        Application excelApp = new();
+        //            try
+        //            {
+        //                if (!File.Exists(path))
+        //                {
+        //                    excelApp.Workbooks.Add();
+        //                    _Worksheet workSheet = (_Worksheet)excelApp.ActiveSheet;
+        //                    Console.WriteLine("Enter number of columns");
+        //                    int numOfColumns = int.Parse(Console.ReadLine());
+        //                    for (int i = 0; i<numOfColumns; i++)
+        //                    {
+        //                        Console.WriteLine("Enter column {0} name: ", i + 1);
+        //                        string? columnName = Console.ReadLine();
+        //                        workSheet.Cells[1, i + 1] = columnName;
+        //                    }
+
+        //                    Console.WriteLine("Enter number of rows you want to enter data for");
+        //                    int numOfRows = int.Parse(Console.ReadLine());
+        //                    for (int i = 1; i <= numOfRows; i++)
+        //                    {
+        //                        for (int j = 1; j <= numOfColumns; j++)
+        //                        {
+        //                            Console.WriteLine("Enter data for {0}", workSheet.Cells[1, j]);
+        //                            string? data = Console.ReadLine();
+        //                             workSheet.Cells[i + 1, j] = data;
+        //                        }
+        //                    }
+        //                    workSheet.SaveAs(path);
+        //                }
+        //                else
+        //                {
+        //                    var workbook = excelApp.Workbooks.Open(path);
+        //                    _Worksheet workSheet = (_Worksheet)workbook.Sheets[1];
+        //                    int row = workSheet.UsedRange.Rows.Count;
+        //                    int col = workSheet.UsedRange.Columns.Count;
+
+        //                    Console.WriteLine("Enter number of rows you want to enter data for");
+        //                    int numOfRows = int.Parse(Console.ReadLine());
+        //                    for (int i = row + 1; i <= numOfRows; i++)
+        //                    {
+        //                        for (int j = 1; j <= col; j++)
+        //                        {
+        //                           Console.WriteLine("Enter data for {0}", workSheet.Cells[1, j]);
+        //                           string? data = Console.ReadLine();
+        //                           workSheet.Cells[i + 1, j] = data;
+        //                        }
+        //                    }
+        //                    workSheet.SaveAs(path);
+        //}
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    Console.WriteLine(ex.ToString());
+        //                }
     }
 }
--- Program.cs
+++ Program.cs
@@ -14,24 +14,25 @@ namespace FileOperations
 
             do
             {
-                Console.WriteLine("-------------------------------");
+                Console.WriteLine("\n-------------------------------");
                 Console.WriteLine("\t\tFile Operations");
                 Console.WriteLine("-------------------------------");
                 Console.WriteLine("1. Upload file");
                 Console.WriteLine("2. Write data in the file");
-                Console.WriteLine("3. Check available files to write");
+                Console.WriteLine("3. Show uploaded files");
+                Console.WriteLine("4. Check available files to write");
                 Console.WriteLine("-------------------------------");
-                Console.WriteLine("or enter exit to stop");
+                Console.WriteLine("or enter exit to stop\n");
 
                 program.choice = Console.ReadLine();
 
                 switch(program.choice)
                 {
-                    case "1": Console.WriteLine("Enter the file name with path");
+                    case "1": Console.WriteLine("\nEnter the file name with path");
                         program.path = Console.ReadLine();
                         if (string.IsNullOrEmpty(program.path))
                         {
-                            Console.WriteLine("File path should not be null");
+                            Console.WriteLine("\nFile path should not be null");
                             break;
                         }
                         fileOperation.CheckFileExtension(program.path);
@@ -40,30 +41,47 @@ namespace FileOperations
                     case "2":
                         if (fileOperation.files.Count == 0)
                         {
-                            Console.WriteLine("No files are avilable to write. Please add files first.");
+                            Console.WriteLine("\nNo files are avilable to write. Please add files first.");
                             break;
                         }
 
-                        Console.WriteLine("Enter file that you want to write?");
+                        Console.WriteLine("\nEnter file that you want to write?");
                         program.path = Console.ReadLine();
-                        fileOperation.WriteFile(program.path, Path.GetExtension(program.path));
+                        if (Path.IsPathFullyQualified(program.path))
+                            fileOperation.WriteFile(program.path, Path.GetExtension(program.path));
+                        else
+                            Console.WriteLine("Enter complete path...");
                     break;
 
                     case "3":
                         if (fileOperation.files.Count == 0)
                         {
-                            Console.WriteLine("No files are avilable to write.");
+                            Console.WriteLine("\nNo files are uploaded.");
                             break;
                         }
-                        Console.WriteLine("Available files: ");
+                        Console.WriteLine("\nUploaded files: ");
                         foreach(string file in fileOperation.files)
                         {
                             Console.WriteLine(file);
                         }
                         break;
+                    case "4":
+                        if (fileOperation.files.Count == 0)
+                        {
+                            Console.WriteLine("\nNo files are available to write.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nAvailable files to write : ");
+                            foreach (string file in fileOperation.files)
+                            {
+                                if (Path.GetExtension(file) is ".txt")
+                                    Console.WriteLine(file);
+                            }
+                            break;
+                        }
                 }
-
-                
             } while (program.choice.ToLower() != "exit");
         }
     }
