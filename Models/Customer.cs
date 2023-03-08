ff81bdafd7d31375084dd3191ac2e8df4786e0d0
omkarry <yadavomkar505.com>
2023-03-08 16:32:12 +0530

    Initial commit

---
+++ Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI_CRUD_operations.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<Location>? Locations { get; set; }
+    }
+}
