af5faa99e21c60c568f22429f746e405e0e585dc
omkarry <yadavomkar505.com>
2023-03-10 19:12:26 +0530

    Initial commit

---
+++ FilesUpload.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+namespace FileHandling
+{
+    class FilesUpload
+    {
+        public static void UploadFiles(string[] filePaths)
+        {
+            foreach (string filePath in filePaths)
+            {
+                CopyFile(filePath);
+            }
+        }
+
+        public static async Task CopyFile(string filePathToRead)
+        {
+            if (File.Exists(filePathToRead))
+            {
+                await Task.Run(() =>
+                {
+                    string newFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", Path.GetFileName(filePathToRead));
+
+                    using (FileStream fileStreamToWrite = new FileStream(newFilePath, FileMode.Create, FileAccess.Write))
+                    {
+                        using (FileStream fileStreamToRead = new FileStream(filePathToRead, FileMode.Open, FileAccess.Read))
+                        {
+                            fileStreamToRead.CopyTo(fileStreamToWrite);
+                        }
+                    }
+                });
+                Console.WriteLine("File upload Complete: {0}", Path.GetFileName(filePathToRead));
+            }
+            else
+                Console.WriteLine("Fle not found");
+        }
+    }
+}
